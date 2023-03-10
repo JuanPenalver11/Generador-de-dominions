233f3fa00e521aeb07332fce0a99fd55aa89a1d9
Juan Penalver <juanpenalversanchez11@gmail.com>
2023-03-10 20:34:44 +0000

    Dominio

--- app.js
+++ app.js
@@ -12,13 +12,7 @@
     for (let j of adj){
       for (let k of noun){
         for (let l of dominio){
-          result.push(i+j+k+l);echo "# Generador-de-Nombres-de-Dominios" >> README.md
-          git init
-          git add README.md
-          git commit -m "first commit"
-          git branch -M main
-          git remote add origin https://github.com/JuanPenalver11/Generador-de-Nombres-de-Dominios.git
-          git push -u origin main
+          result.push(i+j+k+l);
         }
       }
     }
